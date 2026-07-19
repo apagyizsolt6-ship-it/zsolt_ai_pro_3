943ed7cd87e64387b0d0e4945db510e9f3c7f9b3
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-07-19 15:58:43 +0200

    Update app_colors.dart

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,6 +1,16 @@
-class AppConstants {
-  static const String appName = 'Zsolt AI PRO 3';
-  static const String version = '1.0.0';
+import 'package:flutter/material.dart';
 
-  static const bool debug = true;
+class AppColors {
+  static const Color primary = Color(0xFF1565C0);
+  static const Color secondary = Color(0xFF42A5F5);
+
+  static const Color background = Color(0xFF121212);
+  static const Color surface = Color(0xFF1E1E1E);
+
+  static const Color success = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFFA000);
+  static const Color error = Color(0xFFE53935);
+
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Color(0xFFBDBDBD);
 }
