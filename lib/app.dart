5a61487f312d8a5216df32cce1c1f801d6b89571
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-08-06 20:31:33 +0200

    Add files via upload

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 // ===========================================
 // ZSOLT AI PRO 3
-// Version: v0.0.7
+// Version: v0.1.0
 // File: lib/app.dart
 // ===========================================
 
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 
 import 'core/constants/app_constants.dart';
 import 'core/theme/app_theme.dart';
-import 'screens/home_screen.dart';
+import 'screens/splash_screen.dart';
 
 class ZsoltAiProApp extends StatelessWidget {
   const ZsoltAiProApp({super.key});
@@ -18,12 +18,10 @@ class ZsoltAiProApp extends StatelessWidget {
     return MaterialApp(
       title: AppConstants.appName,
       debugShowCheckedModeBanner: false,
-
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.light,
-
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
